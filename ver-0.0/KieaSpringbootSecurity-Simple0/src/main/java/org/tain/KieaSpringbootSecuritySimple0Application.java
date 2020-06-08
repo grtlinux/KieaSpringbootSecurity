df400e8daaeec4af691beb131b244e4dd67e838c
grtlinux <grtlinux@gmail.com>
2020-06-08 21:05:52 +0900

    create 2020-06-08

---
+++ ver-0.0/KieaSpringbootSecurity-Simple0/src/main/java/org/tain/KieaSpringbootSecuritySimple0Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaSpringbootSecuritySimple0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaSpringbootSecuritySimple0Application.class, args);
+	}
+
+}
